4baa3ff6423c69ab6641ff4359bea11d455f99dc
Ayush Gupta <ayushgupta01195@gmail.com>
2022-10-25 11:48:30 +0530

    implement random username generator

--- tests/test_passgen.py
+++ tests/test_passgen.py
@@ -21,12 +21,16 @@ class TestPassGen(unittest.TestCase):
                 if char in string.ascii_uppercase + string.punctuation
             )
         )
-    
+
     def test_random_passphrase(self):
         test_phrase = wolfram_vault.random_passphrase()
         for words in test_phrase:
             self.assertGreaterEqual(len(words), 4)
             self.assertTrue(words.isalnum())
+
+    def test_random_username(self):
+        test_username = wolfram_vault.random_username()
+        self.assertTrue(test_username.isalnum())
 
 
 if __name__ == "__main__":
--- wolfram_vault/__init__.py
+++ wolfram_vault/__init__.py
@@ -1,3 +1,3 @@
-from wolfram_vault.passgen import random_password, random_passphrase
+from wolfram_vault.passgen import random_password, random_passphrase, random_username
 
-__all__ = ['random_password', 'random_passphrase']
+__all__ = ['random_password', 'random_passphrase', 'random_username']
